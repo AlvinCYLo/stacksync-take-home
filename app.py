eff4dff56f8b74ba94c9c9bd3d91f549f53473f5
Alvin Lo <alvinlo@Alvins-Air.lan>
2024-05-30 00:23:39 -0700

    testing scripts with libraries

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from flask import Flask, request, jsonify
+import json
+from libraries import libraries
 
 app = Flask(__name__)
 
+#pipenv run flask run -h localhost -p 8080
+#https://www.programiz.com/python-programming/methods/built-in/exec
+
 @app.route("/execute", methods=["POST"])
-def home(json):
-    script = request.json
-    print(json)
-    print(script)
+def execute():
+    env = dict([(lib, locals().get(lib, None)) for lib in libraries]) 
+    try:
+        data = request.json
+        script = data.get('script')
+        exec(script, {"__builtins__": None}, env)
+        resp = env['main']()
+        json.loads(resp)
+        return jsonify(resp)
+    
+    except Exception as e:
+        print(e)
+        return e
---
+++ libraries.py
@@ -0,0 +1,1 @@
+libraries = ['os', 'pandas', 'numpy']
